d054a3229ed438360891ba931471d72a83d4a713
adalbertorsilva <adalbertorsilvajr@gmail.com>
2018-02-18 15:48:28 -0300

    Feature
    - Added mongodb and mongoose to model classes

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
 const express = require('express')
 const bodyParser = require('body-parser')
+const mongoose = require('mongoose')
+require('dotenv').config()
+
+const Call = require('./models/call')
+
+mongoose.connect(process.env.DB_CONNECTION_URL)
+
+const call = new Call(91, 11, 1)
+call.save()
 
 const app = express()
 
--- models/call.js
+++ models/call.js
@@ -1,5 +1,15 @@
-class Call {
+const mongoose = require('mongoose')
+const { Model, Schema } = mongoose
+
+const callSchema = new Schema({
+  origin: Number,
+  destiny: Number,
+  valuePerMinute: Number
+})
+
+class Call extends Model {
   constructor (origin, destiny, valuePerMinute) {
+    super()
     this.origin = origin
     this.destiny = destiny
     this.valuePerMinute = valuePerMinute
@@ -18,4 +28,4 @@ class Call {
   }
 }
 
-module.exports = Call
+module.exports = mongoose.model(Call, callSchema)
