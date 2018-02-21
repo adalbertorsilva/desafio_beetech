c7586928d6eaf5c747bdda3d0181a68d13784007
adalbertorsilva <adalbertorsilvajr@gmail.com>
2018-02-21 12:43:59 -0300

    - Request adjustments

--- controllers/simulation_controller.js
+++ controllers/simulation_controller.js
@@ -4,16 +4,10 @@ const Plan = require('../models/plan')
 
 class SimulationController {
   async runSimulation (req, res) {
-    const plan = await Plan.findById(req.body.plan)
-
-    console.log('O QUE CHEGOU ??????????????? ', req.body)
-
+    const plan = req.body.plan ? await Plan.findById(req.body.plan) : null
     const call = await Call.findOne({origin: req.body.origin, destiny: req.body.destiny})
     const simulation = new Simulation(plan, call, req.body.duration)
-    const responseObject = {
-      callValue: simulation.runSimulation(),
-      showClientModal: !call
-    }
+    const responseObject = {callValue: simulation.runSimulation(), showClientModal: !call}
     res.status(200).send(responseObject)
   }
 }
